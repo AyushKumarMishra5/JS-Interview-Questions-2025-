af7d6b9f71ceb7490dfbf8cf8223a4b99b96995f
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2025-03-06 06:50:39 +0530

    Question #1

---
+++ Question1.js
@@ -0,0 +1,13 @@
+// What are the differnt methods to create an array in javascript?
+// method1:
+const arr = [12, 34, 56, 67]
+console.log(arr)
+//method 2: 
+const arr2 = new Array(22, 54, 68)
+console.log(arr2)
+//method 3:
+const arr3 = Array.of(23, 45, 12)
+console.log(arr3)
+//mrthod 4: 
+const arr4 = Array.from('123')
+console.log(arr4)
