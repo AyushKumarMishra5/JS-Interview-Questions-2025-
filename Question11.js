f6b08ae0e1ea3cae91ff50eeeaa78c4a5a6db75e
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2025-03-06 08:12:05 +0530

    Question #11

---
+++ Question11.js
@@ -0,0 +1,5 @@
+// What are differnt ways to create an object
+// Following are the methods to create an object in javascript they are as follows: object literal, object constructor, object create method.
+let obj = {} 
+let obj1 = new Object();
+let obj3 = Object.create({});
