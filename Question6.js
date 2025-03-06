120149d30a4729af676c77a646b0cf8fbc104f7a
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2025-03-06 07:35:14 +0530

    Question #6

---
+++ Question6.js
@@ -0,0 +1,13 @@
+// How do you shuffel an array?
+// to shuffel an array it scould be done using sort method with math.random
+let nums = [12, 3, 45, 11]
+let shuffel = nums.sort(()=> Math.random() - 0.5)
+console.log(shuffel)
+
+
+// output:
+// [ 45, 11, 12, 3 ]
+// [ 11, 45, 3, 12 ]
+
+let num = Math.random() * 1000
+console.log(Math.floor(num))
