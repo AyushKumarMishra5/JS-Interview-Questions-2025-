457eed39dcc53e2de2648a09a792c43fe22fa6af
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2025-03-06 06:55:05 +0530

    Question #2

---
+++ Question2.js
@@ -0,0 +1,7 @@
+// How to check weather the variable is an array?
+// Solution:
+const arr = [12, 34, 56, 77]
+console.log(Array.isArray(arr))
+console.log(arr instanceof Array)
+
+// if the solution return true than it's correct
