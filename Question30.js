3081429b484ffe82bf6e61f751987b0c5b2e35f2
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2025-03-06 10:21:56 +0530

    Question #30

---
+++ Question30.js
@@ -0,0 +1,10 @@
+// What is the differnece between function declaration and function expression
+function greet()
+{
+    return "Hello World";
+}
+
+const greetExpress = function()
+{
+    return "Hello world";
+}
