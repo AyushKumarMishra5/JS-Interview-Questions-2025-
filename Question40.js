2fb9810502dd608d94f05625e72d6ed43210c947
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2025-03-06 11:33:31 +0530

    Question #40

---
+++ Question40.js
@@ -0,0 +1,8 @@
+// What is Symbol in javascript
+// is a new type of primitive data type introduced in ES6 version
+const sym = Symbol('unique')
+console.log(sym);
+
+// What are template variables
+// Accessing let or const before declaration causes an error
+
