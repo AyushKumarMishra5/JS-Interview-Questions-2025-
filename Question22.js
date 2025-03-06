7744023b868617fc5191dd9c431ef4e1f05acdeb
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2025-03-06 09:29:14 +0530

    Question #22

---
+++ Question22.js
@@ -0,0 +1,12 @@
+// What is the differnece between slice(), substring(), substr()
+//slice()
+let name = "HiMyNameIsAyush";
+console.log("Slice() method: ", name.slice(0, 4))
+console.log("Substring() method: ", name.substring(0, 4))
+console.log("Substr() method: ", name.substr(0, 4))
+
+
+// Output
+// Slice() method:  HiMy
+// Substring() method:  HiMy
+// Substr() method:  HiMy
