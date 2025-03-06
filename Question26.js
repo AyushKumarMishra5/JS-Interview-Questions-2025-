5d9b0f82da68c0ce5df62a5314d76e05b4b3f1af
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2025-03-06 10:01:25 +0530

    Question 26

---
+++ Question26.js
@@ -0,0 +1,11 @@
+// How to reverse a string?
+// we can reverse a string using reverse method
+function reverseString(str)
+{
+    return str.split("").reverse().join("");
+}
+
+console.log("Ayush: ", reverseString("Ayush"));
+
+// Output
+// Ayush:  hsuyA
