1e3c0f4988d1b5380dcedc7acfbb88345d4e560f
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2025-03-06 09:01:36 +0530

    Question #18

---
+++ Question18.js
@@ -0,0 +1,11 @@
+// How do you convert an object into array?
+// We can convery an object to array using the enteries method which return the enteries of object in the form of array.
+let obj = {
+    a: 23,
+    b: 34
+}
+console.log("Converting object to array: ", Object.entries(obj))
+
+// Output
+// Converting object to array:  [ [ 'a', 23 ], [ 
+//     'b', 34 ] ]
