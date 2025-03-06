55a491e2bfe0dabb71d64f48efd2b9779466e58d
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2025-03-06 10:28:29 +0530

    Question #32

---
+++ Question32.js
@@ -0,0 +1,9 @@
+
+// How does arrow function differ from regular function
+const add =(a, b)=> a+b;
+console.log("Sum of 2 numbers is: ", add(23, 45))
+
+// one more example
+setTimeout(()=>{
+    console.log("Arrow function example!")
+}, 5000)
